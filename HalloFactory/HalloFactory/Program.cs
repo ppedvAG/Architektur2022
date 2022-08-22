3546bf80fcd09f3df129c74f972fee0076b32bd4
André <AndreR@ppedv.de>
2022-08-22 11:56:30 +0200

    Factory Pattern

---
+++ HalloFactory/HalloFactory/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.Common;
+
+Console.WriteLine("Hello, World!");
+
+
+string conString = "Server=(localdb)\\mssqllocaldb;Database=Northwnd;Trusted_Connection=true";
+DbProviderFactory factory = System.Data.SqlClient.SqlClientFactory.Instance;
+//DbProviderFactory factory = Oracle.ManagedDataAccess.Client.OracleClientFactory.Instance;
+
+
+
+DbConnection con = factory.CreateConnection();
+con.ConnectionString = conString;
+con.Open();
+
+DbCommand cmd = factory.CreateCommand();
+cmd.Connection = con;
+cmd.CommandText = "SELECT * FROM Employees";
+DbDataReader reader = cmd.ExecuteReader();
+
+while (reader.Read())
+{
+    Console.WriteLine($"{reader.GetString(reader.GetOrdinal("FirstName"))} {reader.GetString(reader.GetOrdinal("LastName"))}");
+}

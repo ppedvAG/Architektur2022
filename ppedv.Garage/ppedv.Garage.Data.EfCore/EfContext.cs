8bcd65469208da046643a3e8bbcd60ae6aaed80c
André <AndreR@ppedv.de>
2022-08-23 11:01:44 +0200

    EfCore config fertig

---
+++ ppedv.Garage/ppedv.Garage.Data.EfCore/EfContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.Garage.Model;
+
+namespace ppedv.Garage.Data.EfCore
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Car> Cars => Set<Car>();
+        public DbSet<Driver> Drivers => Set<Driver>();
+        public DbSet<Location> Locations => Set<Location>();
+
+        private readonly string _conString;
+
+        public EfContext(string conString = "Server=(localdb)\\mssqllocaldb;Database=Garage_DEV;Trusted_Connection=true")
+        {
+            _conString = conString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_conString).UseLazyLoadingProxies();
+        }
+    }
+}

6a3d3eed8db942f43071d0072258f23106b88517
André <AndreR@ppedv.de>
2022-08-23 14:18:09 +0200

    Mit dem Repository den DataLayer entkoppelt

---
+++ ppedv.Garage/ppedv.Garage.Data.EfCore/EfRepository.cs
@@ -0,0 +1,40 @@
+﻿using ppedv.Garage.Model;
+using ppedv.Garage.Model.Contracts;
+
+namespace ppedv.Garage.Data.EfCore
+{
+    public class EfRepository : IRepository
+    {
+        EfContext _context = new EfContext();
+
+        public void Add<T>(T entity) where T : Entity
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : Entity
+        {
+            _context.Set<T>().Remove(entity);
+       }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById<T>(int id) where T : Entity
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public int SaveAll()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Update<T>(T entity) where T : Entity
+        {
+            _context.Set<T>().Update(entity);
+        }
+    }
+}
---
+++ ppedv.Garage/ppedv.Garage.Logic.CarServices/CarManager.cs
@@ -0,0 +1,35 @@
+﻿using ppedv.Garage.Model;
+using ppedv.Garage.Model.Contracts;
+
+namespace ppedv.Garage.Logic.CarServices
+{
+    public class CarManager
+    {
+
+        public IRepository Repository { get; }
+
+        public CarManager(IRepository repository)
+        {
+            Repository = repository;
+        }
+
+        public Location GetLocationWithFastesCars()
+        {
+            return Repository.GetAll<Location>().OrderBy(x => x.Cars.Sum(c => c.KW)).FirstOrDefault();
+        }
+
+        public bool CheckColor(Car car, IEnumerable<string> allowedColor)
+        {
+            if (car == null)
+                throw new ArgumentNullException("car");
+
+            if (car == allowedColor)
+                throw new ArgumentNullException("allowedColor");
+
+            if (string.IsNullOrWhiteSpace(car.Color))
+                throw new ArgumentException("color ist null or empty");
+
+            return allowedColor.Contains(car.Color);
+        }
+    }
+}

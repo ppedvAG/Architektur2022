3306a9815490ff656c29b2fad8dc2aeac43ed991
André <AndreR@ppedv.de>
2022-08-24 16:15:19 +0200

    WebApi

---
+++ ppedv.Garage/ppedv.Garage.UI.Api/Controllers/LocationsController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ppedv.Garage.Model;
+using ppedv.Garage.Model.Contracts.Infrastructure;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ppedv.Garage.UI.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private readonly IUnitOfWork _uow;
+
+        public LocationsController(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
+        // GET: api/<LocationsController>
+        [HttpGet]
+        public IEnumerable<Location> Get()
+        {
+            return _uow.LocationRepository.Query().ToList();
+        }
+
+        // GET api/<LocationsController>/5
+        [HttpGet("{id}")]
+        public Location Get(int id)
+        {
+            return _uow.LocationRepository.GetById(id);
+        }
+
+        // POST api/<LocationsController>
+        [HttpPost]
+        public void Post([FromBody] Location loc)
+        {
+            _uow.LocationRepository.Add(loc);
+            _uow.SaveAll();
+        }
+
+        // PUT api/<LocationsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Location loc)
+        {
+            _uow.LocationRepository.Update(loc);
+            _uow.SaveAll();
+        }
+
+        // DELETE api/<LocationsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(Location loc)
+        {
+            _uow.LocationRepository.Delete(loc);
+            _uow.SaveAll();
+        }
+    }
+}
---
+++ ppedv.Garage/ppedv.Garage.UI.Api/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+using ppedv.Garage.Model.Contracts.Infrastructure;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers(options =>
+{
+    options.OutputFormatters.RemoveType<SystemTextJsonOutputFormatter>();
+    options.OutputFormatters.Add(new SystemTextJsonOutputFormatter(new JsonSerializerOptions(JsonSerializerDefaults.Web)
+    {
+        ReferenceHandler = ReferenceHandler.Preserve
+    }));
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUnitOfWork, ppedv.Garage.Data.EfCore.EfUnitOfWork>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

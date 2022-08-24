66944e0369c8ba2ced924913609f60872d3570f5
André <AndreR@ppedv.de>
2022-08-24 14:37:02 +0200

    MVVM händisch

--- ppedv.Garage/ppedv.Garage.UI.WPF/ViewModels/CarsViewModel.cs
+++ ppedv.Garage/ppedv.Garage.UI.WPF/ViewModels/CarsViewModel.cs
@@ -1,18 +1,48 @@
 ﻿using ppedv.Garage.Logic.CarServices;
 using ppedv.Garage.Model;
+using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace ppedv.Garage.UI.WPF.ViewModels
 {
-    class CarsViewModel
+
+    class CarsViewModel : INotifyPropertyChanged
     {
         CarManager cm = new CarManager(new Data.EfCore.EfUnitOfWork());
+        private Car selectedCar;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
 
         public ObservableCollection<Car> Cars { get; set; }
+
+        public SaveCommand SaveCommand { get; set; }
+
+        public Car SelectedCar
+        {
+            get { return selectedCar; }
+            set
+            {
+                selectedCar = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedCar)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Alter)));
+            }
+        }
+
+        public string Alter
+        {
+            get
+            {
+                if (SelectedCar == null) return "-";
+                return (DateTime.Now.Year - SelectedCar.BuiltDate.Year).ToString();
+            }
+        }
+
 
         public CarsViewModel()
         {
             Cars = new ObservableCollection<Car>(cm.UnitOfWork.CarRepository.Query());
+            SaveCommand = new SaveCommand(cm.UnitOfWork);
         }
     }
 }
---
+++ ppedv.Garage/ppedv.Garage.UI.WPF/ViewModels/SaveCommand.cs
@@ -0,0 +1,28 @@
+﻿using ppedv.Garage.Model.Contracts;
+using System;
+using System.Windows.Input;
+
+namespace ppedv.Garage.UI.WPF.ViewModels
+{
+    class SaveCommand : ICommand
+    {
+        private readonly IUnitOfWork _uow;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public SaveCommand(IUnitOfWork uow)
+        {
+            this._uow = uow;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _uow.SaveAll();
+        }
+    }
+}

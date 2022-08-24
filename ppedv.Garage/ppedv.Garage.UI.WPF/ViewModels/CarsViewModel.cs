4f2701a9b18f13f1fafa79ea22cebb3a5a7f2abe
André <AndreR@ppedv.de>
2022-08-24 14:47:55 +0200

    MVVM Community Toolkit FTW

--- ppedv.Garage/ppedv.Garage.UI.WPF/ViewModels/CarsViewModel.cs
+++ ppedv.Garage/ppedv.Garage.UI.WPF/ViewModels/CarsViewModel.cs
@@ -1,31 +1,35 @@
-﻿using ppedv.Garage.Logic.CarServices;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ppedv.Garage.Logic.CarServices;
 using ppedv.Garage.Model;
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows.Input;
 
 namespace ppedv.Garage.UI.WPF.ViewModels
 {
 
-    class CarsViewModel : INotifyPropertyChanged
+    class CarsViewModel : ObservableObject
     {
         CarManager cm = new CarManager(new Data.EfCore.EfUnitOfWork());
         private Car selectedCar;
 
-        public event PropertyChangedEventHandler? PropertyChanged;
-
         public ObservableCollection<Car> Cars { get; set; }
 
         public SaveCommand SaveCommand { get; set; }
+        public ICommand SaveCommand2 { get; set; }
 
         public Car SelectedCar
         {
             get { return selectedCar; }
             set
             {
-                selectedCar = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedCar)));
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Alter)));
+                //selectedCar = value;
+                SetProperty(ref selectedCar, value);
+                OnPropertyChanged(nameof(Alter));
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedCar)));
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Alter)));
             }
         }
 
@@ -43,6 +47,8 @@ namespace ppedv.Garage.UI.WPF.ViewModels
         {
             Cars = new ObservableCollection<Car>(cm.UnitOfWork.CarRepository.Query());
             SaveCommand = new SaveCommand(cm.UnitOfWork);
+
+            SaveCommand2 = new RelayCommand(() => cm.UnitOfWork.SaveAll());
         }
     }
 }

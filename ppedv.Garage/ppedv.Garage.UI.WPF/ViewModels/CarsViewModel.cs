bf9c21dd676c05744864ee730554cafabd4f2e7f
André <AndreR@ppedv.de>
2022-08-24 14:10:23 +0200

    Binding

---
+++ ppedv.Garage/ppedv.Garage.UI.WPF/ViewModels/CarsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ppedv.Garage.Logic.CarServices;
+using ppedv.Garage.Model;
+using System.Collections.ObjectModel;
+
+namespace ppedv.Garage.UI.WPF.ViewModels
+{
+    class CarsViewModel
+    {
+        CarManager cm = new CarManager(new Data.EfCore.EfUnitOfWork());
+
+        public ObservableCollection<Car> Cars { get; set; }
+
+        public CarsViewModel()
+        {
+            Cars = new ObservableCollection<Car>(cm.UnitOfWork.CarRepository.Query());
+        }
+    }
+}
